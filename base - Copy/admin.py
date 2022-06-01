5de24b6eabafd52cd27f62293b4117ba1f4da0c7
Kinley Yoezer <kyoezer@moic.gov.bt>
2022-06-01 09:15:31 +0600

    cookies

--- base - Copy/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from base.models import ImageGallery
-
-# admin.site.register(ImageGallery)
-
-# Register your models here.
--- base - Copy/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ImageGallery(models.Model):
-    image = models.ImageField(blank=True, upload_to='pics')
-    caption = models.CharField(null=True, max_length=200)
-
-
-# Create your models here.
